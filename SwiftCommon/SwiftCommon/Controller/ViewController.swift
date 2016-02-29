efc42f1ea6b3f1f28a029809ee09d1ad22da87af
yihongmingfeng <2547050802@qq.com>
2016-03-01 00:09:10 +0800

    调整了文件结构

---
+++ SwiftCommon/SwiftCommon/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCommon
+//
+//  Created by 一泓明峰 on 16/2/29.
+//  Copyright © 2016年 RJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
